d60322d7728ce77a4f5dd0d42de76ad566f47e52
surbhi agrawal <agrawalsurbhi1990@gmail.com>
2021-06-12 11:39:21 +0200

    make it better

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"agrawalsurbhi1990.com\"\n\
+email = \"agrawalsurbhi1990@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
